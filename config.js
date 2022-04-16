7e7d84b558153b57f4f03f780a8c57f2966e5dba
LeNamloveKimSa <103820628+LeNamloveKimSa@users.noreply.github.com>
2022-04-16 16:41:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Gửi công chúa của anh :>",
+    introTitle: 'Nguyễn Hoài Anh',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
+    btnIntro: '^^Hihi tình iu^^',
+    title: 'Phải chăng em đã yêu anh ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say anh ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Yêu lắm cơ <33',
+    btnNo: 'Hong đâu anh trai :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    btnReply: 'Bay đi thoi <33',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    messDesc: 'Công chúa cho anh xin lũi nhaa <33',
+    btnAccept: 'Okii luôn <333',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
